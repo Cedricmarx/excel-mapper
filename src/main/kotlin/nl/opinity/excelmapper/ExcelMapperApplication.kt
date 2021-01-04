bffceb67cc744be980f43112ce59f011363e234e
cedricmarx <cedric.marx@ccvlab.eu>
2021-01-04 20:27:36 +0100

    Include Kotlin Spring Boot classes

---
+++ src/main/kotlin/nl/opinity/excelmapper/ExcelMapperApplication.kt
@@ -0,0 +1,11 @@
+package nl.opinity.excelmapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExcelMapperApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExcelMapperApplication>(*args)
+}
---
+++ src/test/kotlin/nl/opinity/excelmapper/ExcelMapperApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.opinity.excelmapper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExcelMapperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
