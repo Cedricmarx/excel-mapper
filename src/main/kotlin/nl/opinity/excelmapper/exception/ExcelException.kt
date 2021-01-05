1843feb00d393c72c5639b5af7841eae873a9a5e
cedricmarx <cedric.marx@ccvlab.eu>
2021-01-05 13:55:44 +0100

    Add ExcelException to throw exception when something goes wrong while uploading / processing the excel file

---
+++ src/main/kotlin/nl/opinity/excelmapper/exception/ExcelException.kt
@@ -0,0 +1,8 @@
+package nl.opinity.excelmapper.exception
+
+class ExcelException : RuntimeException {
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+}
